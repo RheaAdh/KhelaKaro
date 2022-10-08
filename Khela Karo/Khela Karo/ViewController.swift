800c2fcced84afd70ca1f9b15306fc3f662d9f3f
Rhea Adhikari <rheaadhikari@Ks-iMac.local>
2022-10-08 11:42:23 +0530

    client init

---
+++ Khela Karo/Khela Karo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Khela Karo
+//
+//  Created by Rhea Adhikari on 08/10/22.
+//  Copyright © 2022 Rhea Adhikari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
